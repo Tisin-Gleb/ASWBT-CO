9f9f8d55c84994c805ad6d9103b02f1545357888
Tisin-Gleb <judaskurwa@gmail.com>
2022-12-04 23:03:07 +0700

    start one

---
+++ script/script.js
@@ -0,0 +1,11 @@
+function animate(){
+
+}
+
+function wierdButMouseOn(id){
+    document.getElementById(id).style.backgroundImage = 'linear-gradient(30deg, #022a00, #4e7a02)';
+}
+
+function wierdButMouseOff(id){
+    document.getElementById(id).style.backgroundImage = 'linear-gradient(-75deg, #5200de, #000386)';
+}
